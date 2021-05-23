65c0edc792d83067ed2e29457ed95cb847dc2585
FranciscoPech <fepc087@gmail.com>
2021-05-23 12:39:20 -0500

    segundo commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const port = 3000 | process.env.PORT;
+const port = 3000 || process.env.PORT;
 const server = http.createServer(app);
 
 server.listen(port, () => {
