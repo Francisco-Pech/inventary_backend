44a1b74ab4c202ac8adaa4b7a582c3ecdb68088f
FranciscoPech <fepc087@gmail.com>
2021-05-23 13:04:08 -0500

    commit a

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const app = require('./app');
+const cors = require('cors');
 const port = 3000 || process.env.PORT;
 const server = http.createServer(app);
+app.use(cors());
+
 
 server.listen(port, () => {
     console.log(`Servidor inicializado en el puerto ${port}`)
