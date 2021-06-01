e06ef7764e24d727505ef9a4500e23f7054c4ecf
FranciscoPech <fepc087@gmail.com>
2021-05-31 21:48:51 -0500

    correction cors v1.0.1

--- server.js
+++ server.js
@@ -5,13 +5,9 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 app.use(cors({
     origin: '*',
-    methods: [
-      'GET',
-      'POST',
-      'PUT',
-      'DELETE'
-    ],
-        allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 202
   }));
 
 server.listen(port, () => {
