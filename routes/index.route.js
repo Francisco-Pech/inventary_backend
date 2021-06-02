ceff86f80475e5ff4bbf8cd46470b932f4de3816
FranciscoPech <fepc087@gmail.com>
2021-06-02 11:56:48 -0500

    Backend-V1.0.9

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res) =>{
-    res.json({mensaje : 'Bienvenido a la API REST'});
+    res.json({msg : 'Bienvenido a la API REST'});
 });
 
 module.exports = router
